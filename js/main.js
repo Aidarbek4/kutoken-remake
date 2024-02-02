391ff13d256bd58e104d342a0cf314f1b903276f
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-02-03 04:51:00 +0600

    blog with cards is finished

--- js/main.js
+++ js/main.js
@@ -15,3 +15,32 @@ navbarLinks.forEach((link) => {
         navbarLayer.classList.remove("active");
     })
 })
+
+
+
+function checkVisibility() {
+    const locationMap = document.querySelector('.location__map');
+    const windowHeight = window.innerHeight;
+    const mapPosition = locationMap.getBoundingClientRect();
+
+    // Проверяем, если верхняя граница карты видна и нижняя граница карты не видна
+    const mapTopVisible = mapPosition.top >= 0 && mapPosition.top <= windowHeight;
+    const mapBottomVisible = mapPosition.bottom >= 0 && mapPosition.bottom <= windowHeight;
+
+    if (mapTopVisible || mapBottomVisible) {
+        const places = document.querySelectorAll('.location__map-place');
+        places.forEach(function (place, index) {
+            setTimeout(function () {
+                place.classList.add('visible');
+            }, index * 385); // Задержка в 385 миллисекунд между появлением каждого знака
+        });
+    } else {
+        const places = document.querySelectorAll('.location__map-place');
+        places.forEach(function (place) {
+            place.classList.remove('visible');
+        });
+    }
+}
+
+window.addEventListener('scroll', checkVisibility);
+window.addEventListener('resize', checkVisibility);
